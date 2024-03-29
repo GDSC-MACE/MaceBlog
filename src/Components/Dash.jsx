2bbe3d934202b043abbaa5c635d2cfd7d8efa5ef
Alan Antony <voa.alan@gmail.com>
2024-03-29 10:11:55 +0530

    Dash

--- src/Components/Dash.jsx
+++ src/Components/Dash.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 function Dash() {
   return (
-    <div className="w-[1280px] h-[832px] relative bg-white">
-  <div className="w-[1280px] h-[843px] left-0 top-[-11px] absolute">
-    <div className="w-[1280px] h-[832px] left-0 top-[11px] absolute bg-gradient-to-b from-neutral-800 to-slate-600" />
-    <img className="w-[1280px] h-[841px] left-0 top-0 absolute mix-blend-lighten" src="https://via.placeholder.com/1280x841" />
-  </div>
+    <div className="w-[1280px] h-[832px] relative ">
+  {/* <div className="w-[1280px] h-[843px] left-0 top-[-11px] absolute"> */}
+    {/* <div className="w-[1280px] h-[832px] left-0 top-[11px] absolute bg-gradient-to-b from-neutral-800 to-slate-600" /> */}
+    {/* <img className="w-[1280px] h-[841px] left-0 top-0 absolute mix-blend-lighten" src="https://via.placeholder.com/1280x841" /> */}
+  {/* </div> */}
   <div className="left-[42px] top-[38px] absolute text-orange-500 text-[25px] font-bold font-['DM Sans']">back to home</div>
-  <div className="w-[650px] h-[592px] left-[60px] top-[201px] absolute bg-gradient-to-b from-white to-white rounded-[15px] shadow border-r-8 backdrop-blur-[6px]" />
+  <div className="w-[650px] h-[592px] left-[60px] top-[201px] absolute bg-[linear-gradient(to_right,_rgba(255,255,255,0.12)_75%,_#FF6700_75%)] rounded-xl shadow  backdrop-blur-[6px]" />
   <div className="left-[60px] top-[107px] absolute text-orange-500 text-7xl font-bold font-['DM Sans']">Add new BLOG</div>
   <div className="w-[245px] h-[59px] left-[84px] top-[240px] absolute bg-neutral-800 rounded-xl shadow" />
   <div className="w-[345px] h-[59px] left-[84px] top-[331px] absolute bg-neutral-800 rounded-xl shadow" />
   <div className="w-[445px] h-[264px] left-[84px] top-[424px] absolute bg-neutral-800 rounded-xl shadow" />
   <div className="w-[445px] h-[658px] left-[789px] top-[234px] absolute bg-neutral-800 rounded-xl" />
-  <input type="text" placeholder='enter title' className="left-[120px] top-[252px] absolute bg-neutral-800 text-orange-500 text-opacity-50 text-[25px] font-normal font-['DM Sans'] tracking-[3px]"/>
-  <input type="text" placeholder='enter image url' className="left-[133px] top-[343px] absolute text-orange-500 text-opacity-50 text-[25px] font-normal font-['DM Sans'] tracking-[3px]"/>
-  <input type="text" placeholder='add description' className="left-[121px] top-[436px] absolute text-orange-500 text-opacity-50 text-[25px] font-normal font-['DM Sans'] tracking-[3px]"/>
+  <input type="text" placeholder='enter title' className=" w-[245px] h-[59px] left-[84px] top-[240px] absolute bg-neutral-800 rounded-xl shadow indent-4 text-orange-500 text-opacity-50 text-[25px] font-normal font-['DM Sans'] tracking-[3px]"/>
+  <input type="text" placeholder='enter image url' className=" w-[345px] h-[59px] left-[84px] top-[331px] absolute bg-neutral-800 rounded-xl shadow indent-4 absolute text-orange-500 text-opacity-50 text-[25px] font-normal font-['DM Sans'] tracking-[3px]"/>
+  <input type="text" placeholder='add description' className=" w-[445px] h-[264px] left-[84px] top-[424px] absolute bg-neutral-800 rounded-xl shadow indent-4 absolute text-orange-500 text-opacity-50 text-[25px] font-normal font-['DM Sans'] tracking-[3px]"/>
   <div className="left-[841px] top-[259px] absolute text-orange-500 text-[25px] font-normal font-['DM Sans'] tracking-[3px]">disclaimer</div>
   <div className="left-[842px] top-[336px] absolute text-orange-500 text-xl font-normal font-['DM Sans'] tracking-widest">hdjffdfdfbfjbfjbbfgbfkgkgg<br/>gdfggssssssgfdd<br/>fgffgdddddddddddd<br/>gffdgggggggggggg<br/>gggfdddddddddddd<br/>fgffdgggggggggggggg<br/>gffdgggggggggggggg<br/>gffdggggggggggggg<br/>gfgdddddddddddddd<br/>fggfddddddddddd<br/>fgfgddddddddddddd<br/>fggfdddddddddd<br/>fgfgddddd<br/>fgfgfgfdddddd<br/>fggfgfgfgfgfgfdgfg<br/>fggfgfgfgfgfgfgfg</div>
   <div className="w-[200px] h-14 left-[329px] top-[702px] absolute bg-orange-500 rounded-[10px]" />
