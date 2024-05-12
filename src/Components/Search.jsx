768dec0481d12a90ecbaf072b55e6278af3825d4
Krishnapriya Prasannan <krishnapriyaprasannan1@gmail.com>
2024-05-12 13:29:26 +0530

    some more changes

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -13,8 +13,8 @@ function Search() {
   };
 
   return (
-    <div className="flex flex-col gap-5 text-2xl max-w-[436px] text-white text-opacity-50">
-      <div className="flex flex-auto gap-5 items-start px-5 pt-3 pb-1.5 border-2 border-orange-500 border-solid rounded-[46px]">
+    <div className="flex flex-col  text-2xl max-w-[436px] text-white text-opacity-50">
+      <div className="flex flex-auto px-3 pt-3 pb-1.5 border-2 border-orange-500 border-solid rounded-[46px]">
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/0963866a9a110a604590da4b14f7d70d21dee7a0a705c6aec073640dc4fcdd4c?apiKey=5b229308564b44ab834ca717bcf9317c&"
           alt="Decorative icon"
@@ -27,7 +27,7 @@ function Search() {
           placeholder="Search bloggers..."
           className="flex-auto my-auto bg-transparent border-none outline-none text-white text-opacity-75"
         />
-        <div className="flex gap-5 ml-auto md:ml-3">
+        <div className="flex  ml-auto md:ml-3">
           <button
             onClick={handleFilterClick}
             className="shrink-0 self-start aspect-square w-[60px] focus:outline-none"
