036327560f1b2c4f492c0b9c112ec47d6f7de52e
Krishnapriya Prasannan <krishnapriyaprasannan1@gmail.com>
2024-05-12 13:12:28 +0530

    updated

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -13,7 +13,7 @@ function Search() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row gap-5 text-2xl max-w-[436px] text-white text-opacity-50">
+    <div className="flex flex-col gap-5 text-2xl max-w-[436px] text-white text-opacity-50">
       <div className="flex flex-auto gap-5 items-start px-5 pt-3 pb-1.5 border-2 border-orange-500 border-solid rounded-[46px]">
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/0963866a9a110a604590da4b14f7d70d21dee7a0a705c6aec073640dc4fcdd4c?apiKey=5b229308564b44ab834ca717bcf9317c&"
@@ -27,18 +27,20 @@ function Search() {
           placeholder="Search bloggers..."
           className="flex-auto my-auto bg-transparent border-none outline-none text-white text-opacity-75"
         />
+        <div className="flex gap-5 ml-auto md:ml-3">
+          <button
+            onClick={handleFilterClick}
+            className="shrink-0 self-start aspect-square w-[60px] focus:outline-none"
+          >
+            <img
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/5b1edc13b4e2b4e243f371cacb3d8b840508514ca652d81bf3d3571f7193b900?apiKey=5b229308564b44ab834ca717bcf9317c&"
+              alt="Filter icon"
+            />
+          </button>
+        </div>
       </div>
-      <button
-        onClick={handleFilterClick}
-        className="shrink-0 my-auto aspect-square w-[60px] focus:outline-none"
-      >
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/5b1edc13b4e2b4e243f371cacb3d8b840508514ca652d81bf3d3571f7193b900?apiKey=5b229308564b44ab834ca717bcf9317c&"
-          alt="Filter icon"
-        />
-      </button>
     </div>
   );
 }
 
-export default Search;  
+export default Search;
